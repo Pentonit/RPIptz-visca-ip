a8c57c970997c898bd2c3523e4fced99dcc4d64c
Sebastian Olsen <Sebastian@pentonit.com>
2025-04-11 07:17:55 +0200

    changed stuff

--- run.sh
+++ run.sh
@@ -4,9 +4,9 @@ cd "$(dirname "$0")"
 # Check if virtual environment exists, if not create it
 if [ ! -d "venv" ]; then
     echo "Creating virtual environment..."
-    python3 -m venv venv
+    python3 -m venv venv --system-site-packages
     source venv/bin/activate
-    pip install -r requirements.txt
+    pip install -r requirements.txt --no-deps
 else
     source venv/bin/activate
 fi
